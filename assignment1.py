c173b99f7c02b2bb54d9e666a2a0085d83e5deb3
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-04-01 12:43:16 +1000

    Added two a file and menu method.

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,28 @@
 """
 Replace the contents of this module docstring with your own details
-Name:
-Date started:
-GitHub URL:
+Name: Nishant
+Date started: 30/03/31
+GitHub URL: https://github.com/Nishantranaa/starter_a1_places
 """
 
 
 def main():
-    print("Travel Tracker 1.0 - by <Your Name>")
+    print("Welcome to Travel Tracker version 1.0 - by Nishant_Rana")
+    file()
+
+# if __name__ == '__main__':
 
 
-if __name__ == '__main__':
-    main()
+def file():
+    counter = 0
+    destinations = open("places.csv", "r+")
+    for places in destinations:
+        counter += 1
+    print("{} places loaded from places.csv".format(counter))
+    destinations.close()
+
+def menu():
+    print()
+
+
+main()
