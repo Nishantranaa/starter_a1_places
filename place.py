b882d27348dd4862d6e36a65908a0d13127ec880
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-05-10 10:49:45 +1000

    To add a string function. That when called with an object will return the print format statemented.

--- place.py
+++ place.py
@@ -15,3 +15,5 @@ class Place:
         self.priority = priority
         self.visited_status = visited_status
 
+    def __str__(self):
+        return "{},  country={}, priority={}, visited={}".format(self.name, self.country, self.priority, self.visited_status)
