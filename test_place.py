1e63c9aa73caa1acc3d8785fa4927d2f1a2cd6c8
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-05-10 16:11:20 +1000

    Test to show that the initialization works, using the test print "Success!"

--- test_place.py
+++ test_place.py
@@ -9,22 +9,18 @@ def run_tests():
     """Test Place class."""
 
     # Test empty place (defaults)
-    print("Test empty place:")
+    print("\nTest empty place:")
     default_place = Place()
     print(default_place)
     assert default_place.name == ""
     assert default_place.country == ""
     assert default_place.priority == 0
     assert not default_place.is_visited
+    # TODO: Write tests to show this initialisation works
+    print("Success!")
 
     # Test initial-value place
-    print("Test initial-value place:")
-    new_place = Place("Malagar", "Spain", 1, False)
-    # TODO: Write tests to show this initialisation works
-
-    new_place = Place("Nishant", "Durban", 2, True)
-
-    # TODO: Add more tests, as appropriate, for each method
+    print("\nTest initial-value place:")
 
 
 run_tests()
