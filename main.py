52fdd24229876c42c4fe66a7845e1d6f9c28f66d
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-05-14 19:07:24 +1000

    To created the press_entry function.

--- main.py
+++ main.py
@@ -104,5 +104,51 @@ class TravelTrackerApp(App):
 
         self.visited_status_text = "Places to visit: {}".format(self.pc.get_number_of_unvisited_places())
 
+    def press_clear(self):
+        """
+        Clear any buttons that have been selected (visually) and reset status text
+        :return: None
+        """
+        self.status_text = ""
+        self.visited_status_text = ""
+        self.clear_fields()
+
+    def press_add(self, name, country, priority):
+        """
+        Handler for pressing the add button
+        :return: None
+        """
+        if (not name) or (not country) or (not priority):
+            self.status_text = "All fields must be completed"
+        else:
+
+            try:
+                priority = int(priority)
+                if priority < 1:
+                    self.status_text = "Priority must be > 0"
+                else:
+                    # Adding widget
+                    p = Place(name=name, country=country, priority=priority, visited_status=False)
+                    temp_str = p.__str__() + " (visited)" if p.visited_status else p.__str__()
+                    temp_button = Button(text=temp_str)
+                    temp_button.bind(on_release=self.press_entry)
+                    temp_button.place = p
+                    temp_button.state = "down" if p.visited_status else "normal"
+                    # add the button to the "entries_box" using add_widget()
+                    self.root.ids.entries_box.add_widget(temp_button)
+                    self.clear_fields()
+                    # Adding to place collection
+                    self.pc.add_place(p)
+            except:
+                self.status_text = "Please enter a valid number"
+
+    def clear_fields(self):
+        """
+        Clear the text input fields
+        """
+        self.root.ids.name_input.text = ""
+        self.root.ids.country_input.text = ""
+        self.root.ids.priority_input.text = ""
+
 
 TravelTrackerApp().run()
